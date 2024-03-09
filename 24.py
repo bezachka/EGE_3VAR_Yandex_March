a3515da07c390cb9ea69686a1ff780a1d9c8cafe
bezachka <ilyaershov135@gmail.com>
2024-03-09 18:42:17 +0300

    commit

---
+++ 24.py
@@ -0,0 +1,14 @@
+
+
+alph = ['ABA', 'BAB']
+count = 0
+ms = 0    
+for s in range(3):    
+    for i in range(s, len(f) - 2, 3):
+        if f[i:i+3] in alph:
+            count += 1
+            ms = max(ms, count)
+        else:
+            count = 0
+
+print(ms)
